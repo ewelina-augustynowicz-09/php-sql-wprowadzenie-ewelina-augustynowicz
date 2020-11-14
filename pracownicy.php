9430d48690d1a281c3c3cd0879369adabba3633e
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-14 16:01:39 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ewelina";
+$servername = "mysql-ewelina.alwaysdata.net";
+$username = "ewelina";
+$password = "ewelinailukas";
+$dbname = "ewelina_09";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -11,12 +11,15 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
+
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div>
 <a class="nav_link" href="index.php"> Strona głowna</a><br />
 <a class="nav_link" href="pracownicy.php"> Pracownicy </a><br />
 <a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a><br />
