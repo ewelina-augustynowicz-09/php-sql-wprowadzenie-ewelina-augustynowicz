ec919f81be882279f32f8e721983e5b4990e4b62
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-14 16:33:57 +0100

    Create dataiczas.php

---
+++ dataiczas.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "mysql-ewelina.alwaysdata.net";
+$username = "ewelina";
+$password = "ewelinailukas";
+$dbname = "ewelina_09";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+?>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div>
+<a class="nav_link" href="index.php"> Strona głowna</a><br />
+<a class="nav_link" href="pracownicy.php"> Pracownicy </a><br />
+<a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a><br />
+<a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a><br />
+<a class="nav_link" href="dataiczas.php"> Data i czas</a><br />
+</div>
+   
+<h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
+    <h1><b>Data i czas</b></h1>
+
+</html>
