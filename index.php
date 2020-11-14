a4d7b8a66c289a257cbaf6dadc44484534862924
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-14 15:38:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ewelina";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>TEST</h1>
-</body>
+<a class="nav_link" href="index.php"> Strona głowna</a><br />
+<a class="nav_link" href="pracownicy.php"> Pracownicy </a><br />
+<a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a><br />
+<a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a><br />
+<a class="nav_link" href="dataiczas.php"> Data i czas</a><br />
+</div>
+   
+<h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
+    <h1><b>Strona główna</b></h1>
 
-</html>
+<?php
+$sql = 'SELECT * FROM pracownicy';
+echo ("<p>zadanie 1 </p>");
+echo ("<p>select * from pracownicy");
+$result = $conn->query($sql);
+echo("<table>");
+echo("<th>id</th>");
+echo("<th>imie</th>");
+echo("<th>dzial</th>");
+echo("<th>zarobki</th>");
+echo("<th>data urodzenia</th>");
+
+while($row = $result->fetch_assoc()) {
+echo("<tr>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+echo("</tr>");
+}
+echo("</table>")
+
+
+
+?>
