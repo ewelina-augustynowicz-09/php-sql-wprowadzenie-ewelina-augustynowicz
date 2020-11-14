faf9ac255426bbf61c5b53da4301373aced5767d
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-14 17:30:52 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -67,3 +67,17 @@ echo("</tr>");
 echo("</table>");
 ?>
         
+<?php
+$sql = 'SELECT sum(zarobki) as suma from pracownicy where imie not like "%a" and (dzial=2 or dzial=3)';
+echo ("<h2><p>zadanie 3 - Suma zarobków mężczyzn pracujących w dziale 2 i 3 </p></h2>");
+echo ("<p>select sum(zarobki) as suma from pracownicy where imie not like "%a" and (dzial=2 or dzial=3)");    
+$result = $conn->query('SELECT sum(zarobki) as suma from pracownicy where imie not like "%a" and (dzial=2 or dzial=3)');
+echo("<table>");
+echo("<th>Suma</th>");
+while($row=$result->fetch_assoc()){ 
+echo("<tr>");
+echo("<td>".$row["suma"]."</td>"); 
+echo("</tr>");
+}
+echo("</table>");
+?>
