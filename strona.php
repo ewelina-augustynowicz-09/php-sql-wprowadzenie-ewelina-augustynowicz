9c70787c8c75724859caa2d39cdae4b9c36d2823
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-17 13:36:02 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,6 +1,24 @@
-<?php
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div><br>
+<a class="nav_link" href="index.php"> Strona głowna</a><br />
+<a class="nav_link" href="pracownicy.php"> Pracownicy </a><br />
+<a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a><br />
+<a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a><br />
+<a class="nav_link" href="dataiczas.php"> Data i czas</a><br />
+<a class="nav_link" href="formularz.html">Formularz</a><br />
+</div>
+   
+<h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
+    <h1><b>Jesteś na strona.php</b></h1>
 
-echo("jesteś na strona.php");
+
+</html>
+
+<?php
 
 echo($_POST["imie"]);
 echo($_POST["nazwisko"]);
