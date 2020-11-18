0d13616a6d0bde107cccda76b07d3f07bbe96f0a
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-18 14:39:09 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -24,3 +24,30 @@ echo($_POST["imie"]);
 echo($_POST["nazwisko"]);
      
 ?>
+
+<?php
+
+$servername = "mysql-ewelina.alwaysdata.net";
+$username = "ewelina";
+$password = "ewelinailukas";
+$dbname = "ewelina_09";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql ="INSERT INTO MyGuests (fristname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) --- TRUE {
+echo "New record created successfully";
+} else {
+echo "Error: " .$sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
