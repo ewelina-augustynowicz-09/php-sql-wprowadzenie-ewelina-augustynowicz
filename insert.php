921e5c6975c9bb7a09598c7f3ca4a6e47d5b5215
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-20 13:32:03 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -49,3 +49,42 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 ?>
+
+<?php
+echo("jestes w insert.php <br>");
+echo "<li>". $_POST['name'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
+	      VALUES (
+					null,". 
+					$_POST['name'], 
+					$_POST['dzial'], 
+					$_POST['zarobki'],
+					$_POST['data_urodzenia']."
+				";
+
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
