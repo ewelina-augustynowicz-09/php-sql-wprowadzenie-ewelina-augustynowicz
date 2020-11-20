675bf6f777e70e4e8262bdc8f8e132ec514d3266
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-20 13:35:31 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,3 +1,23 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div><br>
+<a class="nav_link" href="index.php"> Strona głowna</a><br />
+<a class="nav_link" href="pracownicy.php"> Pracownicy </a><br />
+<a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a><br />
+<a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a><br />
+<a class="nav_link" href="dataiczas.php"> Data i czas</a><br />
+<a class="nav_link" href="formularz.html">Formularz</a><br />
+<a class="nav_link" href="insert.php">Insert</a><br />
+<a class="nav_link" href="daneDoBazy.html">Formularz</a><br />
+</div>
+   
+<h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
+    <h1><b>Insert</b></h1>
+</html>
+
 <?php
 $servername = "mysql-ewelina.alwaysdata.net";
 $username = "ewelina";
