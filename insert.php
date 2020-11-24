0b7215c632431a9646bce8d5ae37b6aa3a40e83e
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-24 11:26:58 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -18,28 +18,6 @@
     <h1><b>Insert</b></h1>
 </html>
 
-<?php
-$servername = "mysql-ewelina.alwaysdata.net";
-$username = "ewelina";
-$password = "ewelinailukas";
-$dbname = "ewelina_09";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-//informacja o ewentualnych błędach
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
 
 <?php
 echo("jestes w insert.php");
@@ -70,41 +48,3 @@ if ($conn->query($sql) === TRUE) {
 $conn->close();
 ?>
 
-<?php
-echo("jestes w insert.php <br>");
-echo "<li>". $_POST['name'];
-echo "<li>". $_POST['dzial'];
-echo "<li>". $_POST['zarobki'];
-echo "<li>". $_POST['data_urodzenia'];
-
-$servername = "mysql-ewelina.alwaysdata.net";
-$username = "ewelina";
-$password = "ewelinailukas";
-$dbname = "ewelina_09";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-//definiujemy zapytanie $sql
-$sql = "INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
-	      VALUES (
-					null,". 
-					$_POST['name'], 
-					$_POST['dzial'], 
-					$_POST['zarobki'],
-					$_POST['data_urodzenia']."
-				";
-
-//wyświetlamy zapytanie $sql
-echo "<li>". $sql;
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
