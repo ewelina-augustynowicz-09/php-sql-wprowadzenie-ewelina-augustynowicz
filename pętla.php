8254b8f4886fd6ca52c984c4fca07b3d78064470
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-24 13:10:04 +0100

    Update pętla.php

--- pętla.php
+++ pętla.php
@@ -1,12 +1,13 @@
 <?php
 echo("Pętla");
-function petla($liczba){
+function nazwa($liczba){
    echo("<li>funkcja");
    echo('<li>test ".$liczba);
-   for($i=1;$i<10;$i++){
+   for($i=1;$i<$liczba;$i++){
         echo("<li>to jest w pętli: ".$i);
    }
 }
-petla(1564);
+
+nazwa(90);
 
 ?>
