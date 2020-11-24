bdf3dc331f4959dabe1cc380764024c9cec480e4
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-24 13:18:21 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -48,3 +48,21 @@ if ($conn->query($sql) === TRUE) {
 $conn->close();
 ?>
 
+<?php
+
+echo("<li>imię: ".$_POST['imie']."</li>");
+echo("<li>dział: ".$_POST['dzial']."</li>");
+echo("<li>zarobki: ".$_POST['zarobki']."</li>");
+echo("<li>data urodzenia: ".$_POST['data_ur']."</li>");
+
+
+$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_ur']."')";
+	
+if ($conn->query($sql) === TRUE) {
+echo("<p class='precord'>Nowy pracownik został dodany</p>");
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
+
