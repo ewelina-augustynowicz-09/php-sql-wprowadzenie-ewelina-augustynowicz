450b535b487d4f269e3752f79b814ccd66581390
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-27 13:45:35 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -39,7 +39,7 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
+$sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy']."";
 
 echo $sql;
 
