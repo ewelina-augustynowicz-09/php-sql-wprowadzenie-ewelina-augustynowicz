46d86fb9e6fa7b547b5e495e019893742b07a49c
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-11-27 13:22:03 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -45,7 +45,7 @@ echo $sql;
 
 if ($conn->query($sql) === TRUE) {
   echo "Record deleted successfully";
-  header ('https://ewelina09.herokuapp.com/daneDoBazy.php');
+  header ('Location:https://ewelina09.herokuapp.com/daneDoBazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
