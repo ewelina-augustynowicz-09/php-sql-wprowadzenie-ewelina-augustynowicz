bcb54e669f4bc7b67e3c03b6b9e5cf720beae7f3
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-04 13:21:38 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -66,7 +66,7 @@ echo("</select");
 
 <?php
 $sql = ('SELECT * FROM bibliotekaAT, bibliotekaAutor, bibliotekaTytuł WHERE id_autor=bibliotekaAutor_ID AND id_tytuł=bibliotekaTytul_ID');
-echo("<h1 class=SQL_excercise>Wszystko</h1>");
+echo("<h2>Wszystko</h2>");
 $result=$conn->query($sql);
 echo("<table border=1>");
 echo("<th>id</th>");
