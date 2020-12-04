f649fe08777b0af2e6956718476fa25dffc6a23b
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-04 08:00:25 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "mysql-ewelina.alwaysdata.net";
+$username = "ewelina";
+$password = "ewelinailukas";
+$dbname = "ewelina_09";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+?>
