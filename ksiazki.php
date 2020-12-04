477fca5141ab17bd7be6d0e6ba4b68e36b625912
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-04 08:53:29 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -13,6 +13,30 @@ if (!$conn) {
 }
 ?>
 
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div><br>
+<a class="nav_link" href="index.php"> Strona głowna</a><br />
+<a class="nav_link" href="pracownicy.php"> Pracownicy </a><br />
+<a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a><br />
+<a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a><br />
+<a class="nav_link" href="dataiczas.php"> Data i czas</a><br />
+<a class="nav_link" href="formularz.html">Formularz</a><br />
+<a class="nav_link" href="insert.php">Insert</a><br />
+<a class="nav_link" href="daneDoBazy.php">Dane do bazy</a><br />
+<a class="nav_link" href="petla.php">Pętla</a><br />    
+<a class="nav_link" href="delete.php">Delete</a><br /> 
+<a class="nav_link" href="ksiazki.php">Książki</a><br /> 
+</div>
+   
+<h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
+    <h1><b>Książki</b></h1>
+
+
+</html>
 
 <?php
 echo("<h2>Autorzy</h2>");
@@ -27,6 +51,7 @@ echo("<td>".$row["id_autor"]."</td><td>".$row["Autor"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
+?>
 
 <?php
 $sql = ("SELECT * FROM bibliotekaTytuł");
