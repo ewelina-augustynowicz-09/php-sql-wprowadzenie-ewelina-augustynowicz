27adb1d6ee3c8f18157b535762d2cfd0201a89e0
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-04 08:10:23 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -12,3 +12,69 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
+<?php
+echo("<h1>Książki i autorzy:</h1>");
+$sql = 'SELECT * FROM biblTytul';
+echo("<h2>Tytuły:</h2>");
+$result = $conn->query($sql);
+echo("<table>");
+echo("<th>ID</th>");
+echo("<th>Tytuł</th>");
+while($row=$result->fetch_assoc()){ 
+echo("<tr>");
+echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>"); 
+echo("</tr>");
+}
+echo("</table>");
+?>
+  
+<?php
+$sql = 'SELECT * FROM biblAutor';
+echo("<h2>Autorzy:</h2>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table>");
+echo("<th>ID</th>");
+echo("<th>Tytuł</th>");
+while($row=$result->fetch_assoc()){ 
+echo("<tr>");
+echo("<td>".$row["id"]."</td><td>".$row["autor"]."</td>"); 
+echo("</tr>");
+}
+echo("</table>");
+?>
+
+<?php
+$sql = 'SELECT * FROM biblAutor_biblTytul';
+echo("<h2>ID:</h2>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table>");
+echo("<th>ID</th>");
+echo("<th>ID Autor</th>");
+echo("<th>ID Tytuł</th>");
+while($row=$result->fetch_assoc()){ 
+echo("<tr>");
+echo("<td>".$row["id"]."</td><td>".$row["biblAutor_id"]."</td><td>".$row["biblTytul_id"]."</td>"); 
+echo("</tr>");
+}
+echo("</table>");
+?>
+  
+<?php
+$sql = 'SELECT * FROM biblAutor, biblTytul, biblAutor_biblTytul WHERE biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id';
+echo("<h2>Cała tabelka:</h2>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table>");
+echo("<th>ID</th>");
+echo("<th>Autor</th>");
+echo("<th>Tytuł</th>");
+while($row=$result->fetch_assoc()){ 
+echo("<tr>");
+echo("<td>".$row["id"]."</td><td>".$row["autor"]."</td><td>".$row["tytul"]."</td>"); 
+echo("</tr>");
+}
+echo("</table>");        
+?>
