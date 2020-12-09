f81c697883a73732ffba20c2965ca3a8322c551e
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-09 14:39:49 +0100

    Update daneDoBazy.php

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -64,10 +64,10 @@
 </html>
 
 <?php
-$servername = "mysql-ewelina.alwaysdata.net";
-$username = "ewelina";
-$password = "ewelinailukas";
-$dbname = "ewelina_09";
+$servername = "mysql-ewelina09.alwaysdata.net";
+$username = "ewelina09";
+$password = "Ewelina@123456";
+$dbname = "ewelina_09_09";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
