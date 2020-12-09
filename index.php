5d9a768a817dd50ca0ddce9fa28f955333ea112e
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-09 14:29:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,18 +20,20 @@ if (!$conn) {
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+  <ul>
 <div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div><br>
-<a class="nav_link" href="index.php"> Strona głowna</a><br />
-<a class="nav_link" href="pracownicy.php"> Pracownicy </a><br />
-<a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a><br />
-<a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a><br />
-<a class="nav_link" href="dataiczas.php"> Data i czas</a><br />
-<a class="nav_link" href="formularz.html">Formularz</a><br />
-<a class="nav_link" href="insert.php">Insert</a><br />
-<a class="nav_link" href="daneDoBazy.php">Dane do bazy</a><br />
-<a class="nav_link" href="petla.php">Pętla</a><br />    
-<a class="nav_link" href="delete.php">Delete</a><br /> 
-<a class="nav_link" href="ksiazki.php">Książki</a><br /> 
+<li><a class="nav_link" href="index.php"> Strona głowna</a></li>
+<li><a class="nav_link" href="pracownicy.php"> Pracownicy </a></li>
+<li><a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a></li>
+<li><a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a></li>
+<li><a class="nav_link" href="dataiczas.php"> Data i czas</a></li>
+<li><a class="nav_link" href="formularz.html">Formularz</a></li>
+<li><a class="nav_link" href="insert.php">Insert</a></li>
+<li><a class="nav_link" href="daneDoBazy.php">Dane do bazy</a></li>
+<li><a class="nav_link" href="petla.php">Pętla</a></li>    
+<li><a class="nav_link" href="delete.php">Delete</a></li> 
+<li><a class="nav_link" href="ksiazki.php">Książki</a></li> 
+  </ul>
 </div>
    
 <h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
