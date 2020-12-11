2c4e7703ce2c1203357e7771f193caf335fedd99
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-11 12:56:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,15 +30,25 @@ if (!$conn) {
 <div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div>
   <br>
 <a class="nav_link" href="index.php"> Strona głowna</a>
+  <br>
 <a class="nav_link" href="pracownicy.php"> Pracownicy </a>
+    <br>
 <a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a>
+    <br>
 <a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a>
+    <br>
 <a class="nav_link" href="dataiczas.php"> Data i czas</a>
+    <br>
 <a class="nav_link" href="formularz.html">Formularz</a>
+    <br>
 <a class="nav_link" href="insert.php">Insert</a>
+    <br>
 <a class="nav_link" href="daneDoBazy.php">Dane do bazy</a>
+    <br>
 <a class="nav_link" href="petla.php">Pętla</a>
+    <br>
 <a class="nav_link" href="delete.php">Delete</a> 
+    <br>
 <a class="nav_link" href="ksiazki.php">Książki</a>
   </ul>
   </div>
