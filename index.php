a4213a271732f2db01cf594a3cab8a822765ddda
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-11 13:55:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,50 +25,53 @@ if (!$conn) {
 </head>
 <body>
 <div class="container"
-   <div class="item colorBlue">menu boczne
+  <div class="item colorRed">
+  <div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div>
+  </div>
+  
+  <div class="item colorBlue">menu boczne
   <ul>
-<div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div>
   <br>
-    <br>
-<a class="nav_link" href="index.php"> Strona głowna</a>
   <br>
-    <br>
-<a class="nav_link" href="pracownicy.php"> Pracownicy </a>
-    <br>
-    <br>
-<a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a>
-    <br>
-    <br>
-<a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a>
-    <br>
-    <br>
-<a class="nav_link" href="dataiczas.php"> Data i czas</a>
-    <br>
-    <br>
-<a class="nav_link" href="formularz.html">Formularz</a>
-    <br>
-    <br>
-<a class="nav_link" href="insert.php">Insert</a>
-    <br>
-    <br>
-<a class="nav_link" href="daneDoBazy.php">Dane do bazy</a>
-    <br>
-    <br>
-<a class="nav_link" href="petla.php">Pętla</a>
-    <br>
-    <br>
-<a class="nav_link" href="delete.php">Delete</a> 
-    <br>
-    <br>
-<a class="nav_link" href="ksiazki.php">Książki</a>
+  <a class="nav_link" href="index.php"> Strona głowna</a>
+  <br>
+  <br>
+  <a class="nav_link" href="pracownicy.php"> Pracownicy </a>
+  <br>
+  <br>
+  <a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a>
+  <br>
+  <br>
+  <a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a>
+  <br>
+  <br>
+  <a class="nav_link" href="dataiczas.php"> Data i czas</a>
+  <br>
+  <br>
+  <a class="nav_link" href="formularz.html">Formularz</a>
+  <br>
+  <br>
+  <a class="nav_link" href="insert.php">Insert</a>
+  <br>
+  <br>
+  <a class="nav_link" href="daneDoBazy.php">Dane do bazy</a>
+  <br>
+  <br>
+  <a class="nav_link" href="petla.php">Pętla</a>
+  <br>
+  <br>
+  <a class="nav_link" href="delete.php">Delete</a> 
+  <br>
+  <br>
+  <a class="nav_link" href="ksiazki.php">Książki</a>
   </ul>
-
+  </div
   
   <div class="item colorGreen">
-<h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
-    <h1><b>Strona główna</b></h1>
+  <h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
+  <h1><b>Strona główna</b></h1>
   </div>
-  </div>
+  
 </div>
 
 </body>
