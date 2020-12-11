46f43b7fd6feed99292526877c0f28b5022fef11
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-11 09:25:47 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -16,6 +16,10 @@ if (!$conn) {
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<title>Książki</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
