246b70fca8bffa8e273911a815227224369726cf
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-11 09:16:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,10 @@ if (!$conn) {
 
 <html>
 <head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<title>menu</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
