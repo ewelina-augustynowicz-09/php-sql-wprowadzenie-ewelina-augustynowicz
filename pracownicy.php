6253f796efb3189f81c7ac0d05462475a3bd18c7
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-11 09:19:11 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -16,6 +16,10 @@ if (!$conn) {
 
 <html>
 <head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<title>Pracownicy</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
