497755427b6ef1baba1361b870e60e891322cb45
ewelina-augustynowicz-09 <73532526+ewelina-augustynowicz-09@users.noreply.github.com>
2020-12-18 08:44:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,61 +16,58 @@ if (!$conn) {
 ?>
 
 <html>
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-<title>Strona główna</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-<div class="container"
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Strona główna</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="item colorRed">
+        <div class="nav">
+          <ul>
+            <li>
+              <a
+                href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie"
+                >github</a
+              >
+            </li>
 
-<div class="item colorRed">
-  <ul>
-  <div class="button"><a href="https://github.com/ewelina-augustynowicz-09/sql-php-wprowadzenie">github</a></div>
-  <br>
-  <br>
-  <a class="nav_link" href="index.php"> Strona głowna</a>
-  <br>
-  <br>
-  <a class="nav_link" href="pracownicy.php"> Pracownicy </a>
-  <br>
-  <br>
-  <a class="nav_link" href="pracownicyiorganizacja.php"> Pracownicy i organizacja </a>
-  <br>
-  <br>
-  <a class="nav_link" href="funkcjeAgregujace.php"> Funkcje agregujące </a>
-  <br>
-  <br>
-  <a class="nav_link" href="dataiczas.php"> Data i czas</a>
-  <br>
-  <br>
-  <a class="nav_link" href="formularz.html">Formularz</a>
-  <br>
-  <br>
-  <a class="nav_link" href="insert.php">Insert</a>
-  <br>
-  <br>
-  <a class="nav_link" href="daneDoBazy.php">Dane do bazy</a>
-  <br>
-  <br>
-  <a class="nav_link" href="petla.php">Pętla</a>
-  <br>
-  <br>
-  <a class="nav_link" href="delete.php">Delete</a> 
-  <br>
-  <br>
-  <a class="nav_link" href="ksiazki.php">Książki</a>
-  </ul>
+            <li><a class="nav_link" href="index.php"> Strona głowna</a></li>
 
-  
-<div class="item colorBlue">
-    <h2>Ewelina Augustynowicz<br>klasa 2Ti grupa 2<br>numer 1</h2>
-    <h1><b>Strona główna</b></h1>
-</div>
-</div>
-</div>
+            <li><a class="nav_link" href="pracownicy.php"> Pracownicy </a></li>
 
-</body>
+            <a class="nav_link" href="pracownicyiorganizacja.php">
+              Pracownicy i organizacja
+            </a>
+
+            <a class="nav_link" href="funkcjeAgregujace.php">
+              Funkcje agregujące
+            </a>
+
+            <a class="nav_link" href="dataiczas.php"> Data i czas</a>
+
+            <a class="nav_link" href="formularz.html">Formularz</a>
+
+            <a class="nav_link" href="insert.php">Insert</a>
+
+            <a class="nav_link" href="daneDoBazy.php">Dane do bazy</a>
+
+            <a class="nav_link" href="petla.php">Pętla</a>
+
+            <a class="nav_link" href="delete.php">Delete</a>
+
+            <a class="nav_link" href="ksiazki.php">Książki</a>
+          </ul>
+        </div>
+      </div>
+
+      <div class="item colorBlue">
+        <h2>Ewelina Augustynowicz<br />klasa 2Ti grupa 2<br />numer 1</h2>
+        <h1><b>Strona główna</b></h1>
+      </div>
+    </div>
+  </body>
 </html>
